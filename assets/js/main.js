7f5bcb14368c6c6e111d5bdcb5c81fcf45fbe955
Mikal Morello <mikalmorello@gmail.com>
2019-01-06 21:05:12 -0500

    JS - Updated project feed variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,21 @@
 // VARIABLES
 const xhr = new XMLHttpRequest(),
-      baseUrl = 'https://api.edamam.com/search?q=',
-      appId ='&app_id=d22ae923',
-      appKey ='&app_key=732c0c142c23ebefb242a35d1ff382c3',
+      baseUrl = 'http://dev-tufts-interactive-design.pantheonsite.io/api/projects?_format=json',
       header = document.getElementById('header'),
       searchInput = document.getElementById('searchInput'),
       searchButton = document.getElementById('searchButton'),
-      loadMoreButton = document.getElementById('loadMoreButton'),
-      resultContainer = document.getElementById('projects'),
-      resultCountContainer = document.getElementById('project-results');
+      resultContainer = document.getElementById('projects');
       
-let searchResultRange = 9;
-    searchResultMin = 0,
-    searchResultMax = searchResultMin + searchResultRange,
-    searchInputValue = '',
+let searchInputValue = '',
     searchCount = 0,
     isSearchNew = '';
+
 
 // FUNCTIONS
 
 // API Call
 function callThatAPI(searchParams, isSearchNew) {
-  xhr.open('GET', `${baseUrl}${searchParams}${appId}${appKey}&from=${searchResultMin}&to=${searchResultMax}`);
+  xhr.open('GET', `${baseUrl}`)
   xhr.send();
   xhr.onload = handleSuccess;
   xhr.onerror = handleError;
@@ -31,16 +25,9 @@ function callThatAPI(searchParams, isSearchNew) {
 function handleSuccess() {
   searchCount = searchCount + 1;
   var response = JSON.parse(xhr.responseText);
-  console.log(response);
   loadResults(response);
-  resultCount();
   if (searchCount === 1) {
-    loadMoreButton.classList.add('load-more--show');
     header.classList.remove('header--full-screen');
-    resultCountContainer.classList.add('project-results--show');
-    console.log('first load');
-  } else {
-    console.log('subsequent load');
   }
 }
 
@@ -48,72 +35,39 @@ function handleSuccess() {
 function handleError() {
   console.log('oops');
   resetResults();
-  resultCountContainer.classList.remove('project-results--show');
-  loadMoreButton.classList.remove('load-more--show');
-}
-
-// Inifnite Load count
-
-function infiniteLoad(currentResults) {
-  searchResultMin = currentResults + 1;
-  searchResultMax = searchResultMin + searchResultRange;
-}
-
-// Result Count
-
-function resultCount() {
-  resultCountContainer.innerHTML = `Result count is ${searchResultMax}`;          
-}
-
-// Labels
-function labels(labelArray){
-  var labelList = '';
-  labelArray.forEach(function(element) {
-    labelList += '<span class="project-card__label">' + element + '</span>'; 
-  });
-  console.log(labelList);
-  return labelList;
 }
 
 // Load Results
 
 function loadResults(response){
-  var hits = response.hits;
-  var count = response.count;
-  if(count === 0){
-    resultContainer.innerHTML = `<div class="error">Sorry no results</div>`;
-    resultCountContainer.classList.remove('project-results--show');
-    loadMoreButton.classList.remove('load-more--show');
-  } else {
-    for(let i = 0; i < hits.length; i++) { 
-      resultContainer.innerHTML += 
-       `<article class="project-card slideInUp">
-          <div class="project-card__media">
-            <img src="${hits[i].recipe.image}">
-          </div>
-          <div class="project-card__content">
-            <h2 class="project-card__title">${hits[i].recipe.label}</h2>
-            <div class="project-card__calories">${hits[i].recipe.calories}</div>
-            <div class="project-card__labels">${labels(hits[i].recipe.healthLabels)}</div>
-            <a href="${hits[i].recipe.url}" class="project-card__url">Recipe</a>
-          </div>
-        </article>`;
-     }
-  }
-
+  console.log(response);
+  console.log(response.length);
+  var hits = response;
+  for(let i = 0; i < hits.length; i++) { 
+    let projectTitle =  hits[i].title,
+        projectSubtitle =  hits[i].field_project_subtitle,
+        projectBanner =  hits[i].field_project_banner;
+    resultContainer.innerHTML += 
+     `<article class="project-card slideInUp">
+        <div class="project-card__media">
+          <img src="${projectBanner}">
+        </div>
+        <div class="project-card__content">
+          <h2 class="project-card__title">${projectTitle}</h2>
+          <h3 class="project-card__subtitle">${projectSubtitle}</h3>
+        </div>
+      </article>`;
+   }
 }
 
 // Remove loader icon
 function hideLoader (){
-  
 }
 
 // Reset results
 
 function resetResults() {
   resultContainer.innerHTML = '';
-  searchResultMin = 0;
-  searchResultMax = searchResultMin + searchResultRange;
 }
 
 // Check if new search term
@@ -127,11 +81,11 @@ function checkSearchTerm (newSearchInput){
 }
 
 // Autorun testing if you don't want to render a search
-/*function autoRun(searchValue) {
+function autoRun(searchValue) {
   callThatAPI(searchValue);
-}*/
+}
 
-//autoRun('carrots');
+autoRun('tufts');
 
 // EVENT LISTENER
 
@@ -154,9 +108,3 @@ searchButton.addEventListener('click', function() {
   return searchInputValue;
 });
 
-// Load More Button Click
-loadMoreButton.addEventListener('click', function() {
-  event.preventDefault();
-  infiniteLoad(searchResultMax);
-  callThatAPI(searchInputValue);
-});
